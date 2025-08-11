0125ca59986078cd89fe0a770d8a48ca6dec1f15
VedantNadgir <164067273+VedantNadgir@users.noreply.github.com>
2025-08-11 17:11:57 +0530

    Add files via upload

---
+++ perf.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+class Timer{
+    std::chrono::time_point<std::chrono::system_clock> cur;
+    std::chrono::duration<double> elap;
+    public:
+    Timer() : cur(),elap(std::chrono::duration<double>::zero()){}
+    
+    void start(){
+        cur = std::chrono::system_clock::now(); 
+    }
+    void stop(){
+        elap += std::chrono::system_clock::now()-cur;
+    }
+    void reset(){
+        elap = std::chrono::duration<double>::zero();
+    }
+    double time(){
+        return elap.count();
+    }
+};
+
+
